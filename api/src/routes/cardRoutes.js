0db5d89c81a984403e2af63547657203b7b34074
semestre5grupla <flores.alejandro10moc@gmail.com>
2026-09-03 08:33:22 -0500

    feat: permitir a nivel 2 (Gestor) realizar el borrado lógico de tarjetas

--- api/src/routes/cardRoutes.js
+++ api/src/routes/cardRoutes.js
@@ -16,7 +16,7 @@ router.post('/', requireRoleLevel([1, 2, 3]), cardController.createCard);
 // Administrador, Gerente, Auditor
 router.put('/:id', requireRoleLevel([1, 2, 4]), cardController.updateCard);
 
-// Solo Super Administrador
-router.delete('/:id', requireRoleLevel([1]), cardController.deleteCard);
+// Super Administrador y Gestor
+router.delete('/:id', requireRoleLevel([1, 2]), cardController.deleteCard);
 
 module.exports = router;
